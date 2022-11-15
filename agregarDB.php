3c3faf9dfdb268e5f5bfcd8837e60b665aed3e37
Marcos Vargas Azero <mvadvrock@gmail.com>
2022-11-15 17:21:30 -0400

    configunramos la base de datos y damos estilo a la pagina

--- agregarDB.php
+++ agregarDB.php
@@ -1,43 +1,16 @@
 <?php
-    // $dato=$_POST["titulo"];
-
-    // // conectar a la base
-    // class Conexion{
-    //     private $host = "localhost";
-    //     private $user = "root";
-    //     private $password = "";
-    //     private $db = "tareas";
-    //     private $conect;
-
-        
-
-    //     public function __construct(){
-    //         $conectonString = "mysql:host".$this->host.",dbname=".$this->db."; charset=utf8";
-    //         try {
-    //             $this->conect = new PDO($conectonString, $this->user, $this->password);
-    //             $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     // para detectar mejor los errores
-    //             echo "conectado";                
-    //         } catch (Exception $e) {
-    //             $this->conect = 'Error de conexion';
-    //             echo "Error: ". $e->getMessage();
-    //         }
-    //     }
-    // }
-
-    // $conect = new Conexion();
-
-    $dsn = 'mysql:dbname=tareas;host=localhost';
+    $dsn = 'mysql:dbname=refrescos;host=localhost';
     $usuario = 'root';
     $contraseña = '';
-    $tarea=$_POST["titulo"];
+    $nombre=$_POST["nombre"];
     $id=$_POST["ident"];
 
 
     try {
         $pdo = new PDO($dsn, $usuario, $contraseña);
-        $sql = "INSERT INTO tareas (id,tarea) VALUES (?,?)";
+        $sql = "INSERT INTO refrescos (id,nombre) VALUES (?,?)";
         $stmt= $pdo->prepare($sql);
-        $stmt->execute([$id,$tarea]);
+        $stmt->execute([$id,$nombre]);
         echo "Conectado";
     } catch (PDOException $e) {
         echo 'Falló la conexión: ' . $e->getMessage();
--- eliminarDB.php
+++ eliminarDB.php
@@ -1,12 +1,12 @@
 <?php
-$dsn = 'mysql:dbname=tareas;host=localhost';
+$dsn = 'mysql:dbname=refrescos;host=localhost';
 $usuario = 'root';
 $contraseña = '';
 $id=$_POST["ident"]; 
 echo $id;
     try {
         $pdo = new PDO($dsn, $usuario, $contraseña);
-        $sql = "delete from tareas where id = ".$id;
+        $sql = "delete from refrescos where id = ".$id;
         $stmt= $pdo->prepare($sql);
         $stmt->execute();
     } catch (PDOException $e) {
